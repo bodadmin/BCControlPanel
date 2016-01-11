c54c34bb7b3017a4d4fae96653514b26963da7ba
Will Godsall null <wgodsall@bod.uk.com>
2016-01-11 16:57:51 +0000

    New files added - [UsersController.java,db-queries.hbm.xml,page.min.html,UserManagement.variables.json,UsersServiceImpl.java,DbQueryExecutorServiceImpl.java,db.properties,ProcedureExecutionController.java,DbProcedureExecutorServiceImpl.java,Users.java,db_API.json,UsersService.java,UserManagement.html,db_procedure.json,db.spring.xml,general-options.json,servicedef.xml,DbProcedureExecutorService.java,db-procedures.mappings.json,db_vk.hbm.xml,UserManagement.js,database-options.json,DbQueryExecutorService.java,db-connection-settings.json,db_published_dataModel.json,db.hbm.xml,UsersDao.java,roles.json,db_query.json,QueryExecutionController.java,UserManagement.css] Files modified - [production.properties,project-services.xml,project-security-provider.xml,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/db/src/com/bccontrolpanel/db/service/DbProcedureExecutorService.java
@@ -0,0 +1,21 @@
+/*Copyright (c) 2016-2017 Business on Demand Ltd. All Rights Reserved. This software is the confidential and proprietary information of Business on Demand Ltd. You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with Business on Demand Ltd.*/
+
+
+package com.bccontrolpanel.db.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface DbProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+

ebde61bf515f44b152fb5376a96fb985db6d8fe4
Will Godsall null <wgodsall@bod.uk.com>
2016-01-12 09:45:19 +0000

    New files added - [servicedef.xml,README.md,SettingsService.java,package-info.java,SettingsController.java,SettingsService_API.json,context.txt,SettingsService.spring.xml,context1.txt,uploads] Files modified - [FileService.java,FileController.java,Settings.html,project-services.xml,FileService_API.json,page.min.html,package-info.java] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/SettingsService/src/com/bccontrolpanel/settingsservice/SettingsService.java
@@ -0,0 +1,94 @@
+/*Copyright (c) 2016-2017 Business on Demand Ltd. All Rights Reserved. This software is the confidential and proprietary information of Business on Demand Ltd. You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with Business on Demand Ltd.*/
+
+package com.bccontrolpanel.settingsservice;
+
+import com.wavemaker.runtime.WMAppContext;
+import com.wavemaker.runtime.file.model.DownloadResponse;
+import com.wavemaker.runtime.file.manager.FileServiceManager;
+import com.wavemaker.runtime.util.WMRuntimeUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.activation.MimetypesFileTypeMap;
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a singleton class with all of its public methods exposed to the client via controller.
+ * Their return values and parameters will be passed to the client or taken
+ * from the client respectively.
+ */
+@ExposeToClient
+public class SettingsService {
+
+    private static final Logger logger=LoggerFactory.getLogger(SettingsService.class);
+    
+    @Autowired
+    private FileServiceManager fileServiceManager;
+
+    private File uploadDirectory = null;
+
+    @PostConstruct
+    protected void init() {
+        uploadDirectory = getUploadDir();
+    }
+    
+    protected File getUploadDir() {
+       String uploadDir = WMAppContext.getInstance().getContext().getRealPath("/resources/uploads");
+       File f = new File(uploadDir);
+       f.mkdirs();
+       return f;
+    }
+    
+    public int listFiles(String relativePath) throws IOException {
+        MimetypesFileTypeMap m = new MimetypesFileTypeMap();
+        File[] files = fileServiceManager.listFiles(relativePath == null ? uploadDirectory : new File(uploadDirectory, relativePath));
+        
+        int result = files.length;
+        /* Iterate over every file, creating a WMFile object to be returned */
+        //WMFile[] result = new WMFile[files.length];
+        //for (int i = 0; i < files.length; i++) {
+        //    String filteredPath = WMRuntimeUtils.getContextRelativePath(files[i], httpServletRequest, relativePath);
+        //    result[i] = new WMFile(filteredPath, files[i].getName(), files[i].length(), m.getContentType(files[i]));
+        //}
+        return result;
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
